799ea073d32d20a0f7decbb41e457e99f7dac667
mariliagabii <marilia.gabriely@escolar.ifrn.edu.br>
2024-09-02 16:18:22 -0300

    movimentacao

--- Meu Game 3D/Assets/scripts/player.cs
+++ Meu Game 3D/Assets/scripts/player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    public int velocidade = 10;
+    private Rigidbody rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+      Debug.Log("START");
+      TryGetComponent(out rb);
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+        Debug.Log("UPDATE");
+        float h = Input.GetAxis("Horizontal"); //-1 esquerda,0 nada, 1 direita
+        float v = Input.GetAxis("Vertical"); // -1 pra tras, 0 nada, 1 pra frente
         
+        
+        UnityEngine.Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade);
+
     }
 }
